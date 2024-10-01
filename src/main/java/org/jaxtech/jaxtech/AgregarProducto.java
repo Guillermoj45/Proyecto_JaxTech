892c3d60df71d5f1bd8597a9b8206bf33cb25608
Guillermo <gremesalgarcia@gmail.com>
2024-10-01 22:15:40 +0200

    Foto hecha

---
+++ src/main/java/org/jaxtech/jaxtech/AgregarProducto.java
@@ -0,0 +1,41 @@
+package org.jaxtech.jaxtech;
+
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+
+public class AgregarProducto {
+
+    public TextField nombreTextFiled;
+    public TextField stockTextFiled;
+    public TextField dimensionesTextFiled;
+    public ChoiceBox<String> tipoChoice;
+    public ImageView productoImg;
+    public Image imagen;
+
+    public void initialize() {
+        tipoChoice.getItems().addAll("Teclado", "Ratón", "Camara", "Auriculares", "Monitor", "Portatil", "Sobremesa");
+    }
+
+    public void seleccionarImagen() {
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.getExtensionFilters().addAll(
+                new FileChooser.ExtensionFilter("Image Files", "*.png", "*.jpg", "*.jpeg", "*.gif")
+        );
+        File selectedFile = fileChooser.showOpenDialog(null);
+        if (selectedFile != null) {
+            try {
+                String imagePath = selectedFile.toURI().toString();
+                imagen = new Image(imagePath);
+                productoImg.setImage(imagen);
+            } catch (Exception e) {
+                e.printStackTrace();
+                // Maneja la excepción según sea necesario
+            }
+        }
+    }
+}
--- src/main/java/org/jaxtech/jaxtech/HelloApplication.java
+++ src/main/java/org/jaxtech/jaxtech/HelloApplication.java
@@ -2,6 +2,7 @@ package org.jaxtech.jaxtech;
 
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
@@ -10,8 +11,9 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load());
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/org/jaxtech/jaxtech/agregar_producto.fxml"));
+        Parent root = fxmlLoader.load();
+        Scene scene = new Scene(root);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
